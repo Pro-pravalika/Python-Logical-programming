e06413dbecb750cb99eee6b638d046dca83ed02f
Pravalika Chunarkar <99178842+Pro-pravalika@users.noreply.github.com>
2022-02-20 22:32:54 +0530

    Add files via upload

---
+++ Find maximum value of ascii value.py
@@ -0,0 +1,12 @@
+# to find the maximum value from alphabatical character from a string....
+
+
+def sample(character):
+	print(max(character))
+sample('pravalika')
+
+# to find the minimum value from alphabatical character from a string...
+
+def sample(character):
+	print(min(character)) 
+sample('pravalika')
---
+++ max from special characters.py
@@ -0,0 +1,10 @@
+# finding the max character from the given value..
+
+def sample(value):
+	print(max(value))
+sample('num{ }ber')
+
+
+def sample(value):
+	print(min(value))
+sample('num{}ber')
