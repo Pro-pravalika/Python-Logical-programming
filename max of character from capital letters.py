c72b7a52632e5e0de07e34ca7a1ac28f852865ed
Pravalika Chunarkar <99178842+Pro-pravalika@users.noreply.github.com>
2022-02-20 22:55:21 +0530

    Add files via upload

---
+++ max of character from capital letters.py
@@ -0,0 +1,10 @@
+# to find the max of character from capital letters.
+
+def sample(value):
+	print(max(value))
+sample('ASRDNQWI')
+
+
+def sample(value):
+	print(min(value))
+sample('HYWSAFTWQHYCZ')
